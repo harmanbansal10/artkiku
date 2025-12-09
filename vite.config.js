d918249dc1404f6170f85656501f8ea7a94addf7
harmanbansal10 <your-email@example.com>
2025-12-09 17:51:40 -0500

    Add vite.config.js with correct base path

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
 // vite.config.js
-export default {
-  base: "/", // <--- VERY IMPORTANT
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // <--- VERY important for Vercel deployment
+})
